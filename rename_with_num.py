27f5af523820d7845741c74bba852dd689681c47
Piotr1103 <manjudamin@gmail.com>
2022-12-05 13:07:12 +0800

    抽離圖檔編號重新命名功能，日後待推廣改進

---
+++ rename_with_num.py
@@ -0,0 +1,18 @@
+import os
+import glob
+
+imgDir = input("請輸入圖檔路徑：")
+fmt = input("請輸入欲取得圖檔之格式，不加點：")
+
+il = glob.glob(f'{imgDir}/*.{fmt}')
+"""
+glob.glob()函數取得的位址字串如下：
+目錄名稱\\001.副檔名
+"""
+for fn in il:
+	s = fn.split('\\')
+	n = s[-1].split('-')[-1]
+	s.pop()
+	pre = '\\'.join(s)
+	nn = pre + "\\" + n
+	os.rename(fn, nn)
